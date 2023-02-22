f638e2bfdd863ca8b27936268a9ec59896e8a5ae
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-22 12:59:29 +0500

    init

---
+++ ArraySort/ArraySort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elements = 10;
+            int[] numbers = new int[elements];
+            int minNumber = 0;
+            int maxNumber = 10;
+            int temp;
+
+            Random random = new Random();
+
+            for (int i = 0; i < elements; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber);
+            }
+
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write(numbers[i]+" ");
+            }
+
+            for (int i = 0; i < elements; i++)
+            {
+                for (int j = 0; j < elements-1-i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write(numbers[i]+" ");
+            }
+        }
+    }
+}
